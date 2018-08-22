fb8d043e356807d571be502fffdcced6747d94f7
Muhammet AYAN <muhammet.ayan@hotmail.com>
2018-08-22 14:00:40 +0300

    Deneme amaçlı bazı geçici testler eklendi.

---
+++ Blockchain/Network/net.js
@@ -0,0 +1,14 @@
+﻿
+module.exports.a = function (req, res, next) {
+    res.write("Hello");
+    if (req.url == "/")
+        next();
+    else
+        res.end();
+};
+
+module.exports.b = function (req, res, next) {
+    res.write(" world!");
+    res.end();
+    next();
+};
--- Blockchain/server.js
+++ Blockchain/server.js
@@ -1,8 +1,8 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+var express = require('express');
+var app = express();
+var net = require('./Network/net');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+app.use(net.a);
+app.use(net.b);
+
+app.listen(5000);
